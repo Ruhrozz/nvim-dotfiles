fc4b23febb3ae5fab021b4280990774fb08f0e36
Malofeev Ivan <i.malofeev@expasoft.tech>
2024-06-03 21:07:04 +0700

    feat: focus on trouble window

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,5 @@
 require("trouble").setup()
 
-vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle focus=true<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics focus=true<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols focus=true<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
