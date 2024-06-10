aba70a308a4113c944ca42e720114e91c1d18e63
Ruhrozz <ruhrozz@bk.ru>
2024-06-10 22:11:52 +0700

    feat: close neotree if last window

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,7 +4,7 @@ vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
 require("neo-tree").setup({
-    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "rounded",
     enable_git_status = true,
     enable_diagnostics = true,
